5b184190de4a5cd48eae7fbb4798823fdd566a86
unknown <yazid1bernou@gmail.com>
2024-03-06 08:38:17 +0000

    add structure html from form and table in App

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,70 @@ import {GlobalContext} from "./contextAPI/Global";
 function App() {
   
  
-  const {product} =  useContext(GlobalContext);
+  const {product} =  useContext(GlobalContext);   
 
   return (
     <>
-      <Navbar />
+     {/*  <Navbar />
 
       <h2> The title is : {product}</h2>
       <Input />
-
+ */}
+    
+      <div className='container m-4'>
+          <div className=''  style={{display : "flex" , justifyContent : "space-between"}}>
+             <div>
+              <h5>All products</h5>
+             </div>
+             <button className='btn btn-primary'> Eye icon</button>
+          </div>
+          <div className=''>
+               {/* --------- Form ---------------------*/}
+               <form>
+                    <div class="mb-3">
+                      <label  class="form-label">Title</label>
+                      <input type="text" class="form-control" />
+                   </div>
+                   <div class="mb-3">
+                      <label  class="form-label">Description</label>
+                      <input type="text" class="form-control" />
+                   </div>
+                   <div class="mb-3">
+                      <label  class="form-label">Price</label>
+                      <input type="text" class="form-control" />
+                   </div>
+                   <div class="mb-3">
+                      <label  class="form-label">Image</label>
+                      <input type="text" class="form-control" />
+                   </div>
+                   
+                   <button type="submit" class="btn btn-primary">Submit</button>
+               </form>
+               {/* ------------ Display All products -------------*/}
+              <table class="table">
+                    <thead class="table-light">
+                      <tr>
+                      <th scope="col">#</th>
+                      <th scope="col">Title</th>
+                      <th scope="col">Description</th>
+                      <th scope="col">Price</th>
+                      <th scope="col">Image</th>
+                      <th scope="col">Action</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <td>Mark</td>
+                      <td>Otto</td>
+                      <td>@mdo</td>
+                      <td>@mdo</td>
+                      <td>@mdo</td>
+                      <td>@mdo</td>
+                    </tbody>
+              </table>
+               
+         </div>
+        
+      </div>
     </>
   )
 }
